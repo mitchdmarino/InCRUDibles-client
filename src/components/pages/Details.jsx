8e4b569829dbccde22e675b24ffccf89fbf49c02
“emilykiss” <emilylanekiss@gmail.com>
2022-06-24 14:08:56 -0700

    details and task pages

---
+++ src/components/pages/Details.jsx
@@ -0,0 +1,7 @@
+export default function Details() {
+  return (
+    <div>
+      <h1 className="text-6xl">Details Page</h1>
+    </div>
+  )
+}
---
+++ src/components/pages/Tasks.jsx
@@ -0,0 +1,7 @@
+export default function Tasks() {
+  return (
+    <div>
+      <h1 className="text-6xl">Task Page</h1>
+    </div>
+  )
+}
