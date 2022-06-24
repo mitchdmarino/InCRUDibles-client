0354d1bafa29934abd071627320ac638f0cf1051
seiplet93 <seiplet93@gmail.com>
2022-06-24 19:45:46 -0400

    actual mess

---
+++ src/components/ProfileForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function ProfileForm() {
+  const [form, setForm] = useState({ initialForm });
+  return (
+    <div>
+      <form onSubmit={(e) => submitHandler(e, form, setForm)}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+        />
+        <label htmlFor="color">Color:</label>
+        <select
+          name="color"
+          type="dropdown"
+          id="color"
+          value={form.color}
+          onChange={(e) => setForm({ ...form, color: e.target.value })}
+        >
+          <option value="red">red</option>
+          <option value="green">green</option>
+          <option value="blue">blue</option>
+          <option value="pink">pink</option>
+          <option value="yellow">yellow</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
